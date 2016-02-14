5f835e8ba410414700445011bd7f258fa0f32120
rbburkey <robinsonbburkey@gmail.com>
2016-02-14 17:00:14 -0500

    postgres

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :companies do
     resource :designs
   end
-  
+
   resources :designs
   resources :reviews
   resources :reviews
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'companies/:id' =>'companies#show'
+  
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
